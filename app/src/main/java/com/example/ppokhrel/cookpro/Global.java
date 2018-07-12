e2c117b01c6550a48f4b56de0661cda95656ec21
Seema Neupane <sneupane@tamu.edu>
2018-07-11 19:37:40 -0500

    Initial commit

---
+++ app/src/main/java/com/example/ppokhrel/cookpro/Global.java
@@ -0,0 +1,6 @@
+package com.example.ppokhrel.cookpro;
+
+public class Global {
+    static String recipe = "";
+    static String ingredients = "";
+}
---
+++ app/src/main/java/com/example/ppokhrel/cookpro/Recipe.java
@@ -0,0 +1,17 @@
+package com.example.ppokhrel.cookpro;
+
+
+public class Recipe {
+    String description;
+    String ingredients;
+    String procedure;
+    String title;
+    String link;
+    String imageUrl;
+
+    public Recipe(){
+
+
+    }
+
+}
